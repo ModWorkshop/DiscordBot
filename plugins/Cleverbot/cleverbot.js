9ab390aae8e1bd3d561d1ff926fd0ea408b51e44
Tatsuto <tatsuto@live.de>
2017-10-15 23:12:46 +0300

    Cleverbot update. Updated depencies.Thanks to tzhongyan

--- plugins/Cleverbot/cleverbot.js
+++ plugins/Cleverbot/cleverbot.js
@@ -2,28 +2,19 @@ exports.commands = [
     "talk"
 ]
 
+var AuthDetails = require("../../auth.json");
+
 var cleverbot = require("cleverbot-node");
 talkbot = new cleverbot;
-cleverbot.prepare(function() {});
+talkbot.configure({"botapi": AuthDetails.cleverbot_api_key});
 
 exports.talk = {
     usage: "<message>",
     description: "Talk directly to the bot",
     process: function(bot, msg, suffix) {
         var conv = suffix.split(" ");
-        talkbot.write(conv, function(response) {
-            msg.channel.sendMessage("", {
-                embed: {
-                    color: 0x8698FE,
-                    author: {
-                        name: "Cleverbot",
-                        icon_url: "https://lh5.ggpht.com/DiNbF90a-ecMdyG7c49ARdKdm2mlhLDyNswLcmDm3WM6yDADmMMWtTO1XL96-LCEXIc=w300"
-                    },
-		    timestamp: new Date(),
-                    description: response.message,
-                }
-            }).catch(console.error);
-            msg.react('👌');
+        talkbot.write(suffix, function(response) {
++       	msg.channel.send(response.output);
         })
     }
 }

8d0ff1c17bae1da64b9da0907547fc4362e8789f
Tatsuto <tatsuto@live.de>
2017-10-15 23:24:44 +0300

    xkcd comic fix by tzhongyan

--- plugins/xkcd/xkcd.js
+++ plugins/xkcd/xkcd.js
@@ -7,21 +7,37 @@ exports.xkcd = {
 	usage: "[comic number]",
 	description: "displays a given xkcd comic number (or the latest if nothing specified",
 	process: function(bot,msg,suffix){
-		var url = "http://xkcd.com/";
-		if(suffix != "") url += suffix+"/";
-		url += "info.0.json";
-		require("request")(url,function(err,res,body){
+		var show = (err,res,body) => {
 			try{
 				var comic = JSON.parse(body);
 				msg.channel.send(
-					comic.title+"\n"+comic.img,function(){
-						msg.channel.send(comic.alt)
-				});
+					comic.title +"\n"+
+					comic.img + "\n*" +
+					comic.alt +"*"
+				);
 			}catch(e){
 				msg.channel.send(
 					"Couldn't fetch an XKCD for "+suffix);
 			}
-		});
+		};
+		var url = "http://xkcd.com/";
+		if (suffix == "random"){
+			require("request")(url+"info.0.json", (err, res, body) => {
+				try {
+					var rand = Math.floor(Math.random() * (JSON.parse(body).num +1));
+					msg.channel.send("Your random xkcd commic: "+rand);
+					url += rand + "/info.0.json";
+					require("request")(url,show);
+				} catch(e) {
+					msg.channel.send("Couldn't fetch an XKCD for "+suffix);
+				}
+			});
+		} else if (suffix == "") {
+			require("request")(url + "info.0.json", show);
+		} else {
+			url += suffix + "/info.0.json";
+			require("request")(url,show);
+		}
 	}
 }
 

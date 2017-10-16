098b680ad385372768c5309acaae987ecdebfcaa
noxwyll <tatsuto@live.de>
2017-10-16 16:28:58 +0300

    Check if user actually supplies message. 1st Letter to Uppercase.

--- plugins/Pirate/pirate.js
+++ plugins/Pirate/pirate.js
@@ -7,9 +7,10 @@ exports.commands = [
     exports.pirate = {
         description: "Converts to pirate speak",
         process: function(bot, msg, suffix) {
-			//if(suffix) 
-			var message = pirateSpeak.translate(suffix);
+			if(suffix) {
+				var message = pirateSpeak.translate(suffix);
+				message = message.charAt(0).toUpperCase() + message.slice(1);
 				msg.channel.send(message);
-			//else msg.channel.send("You should give me a message")
+			} else msg.channel.send("You should give me a message.");
         }
     }
